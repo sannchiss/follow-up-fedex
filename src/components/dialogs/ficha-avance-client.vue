d539500b75c8ed229a714cef458979d15626d03b
sannchiss <50926112+sannchiss@users.noreply.github.com>
2023-05-18 10:14:49 -0400

    UPDATE 18-05-2023

---
+++ src/components/dialogs/ficha-avance-client.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+
+    <q-dialog v-model="integracionesStore.dialogoFichaCliente">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Envío de mail ficha</div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-section style="max-height: 50vh" class="scroll">
+          <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit
+            voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem
+            aut, natus minima, porro labore.</p>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions align="right">
+          <q-btn flat label="Decline" color="primary" v-close-popup />
+          <q-btn flat label="Accept" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+  </div>
+</template>
+
+<script>
+
+import { useIntegracionesStore } from 'src/stores/integraciones/integraciones-store'
+
+
+export default {
+
+  setup() {
+    const integracionesStore = useIntegracionesStore()
+
+    return {
+      integracionesStore
+    }
+
+
+  },
+
+
+
+
+}
+</script>
+
+<style></style>
--- src/components/shared/accion-boton-integracion.vue
+++ src/components/shared/accion-boton-integracion.vue
@@ -1,36 +1,27 @@
 <template>
-  <div class="q-pa-md example-row-equal-width">
+  <q-item>
+    <q-item-section>
+      <div class="text-grey-8 q-gutter-xs">
+        <q-btn class="gt-xs" flat dense round v-for="(item, i) in botons" :key="i" :icon="item.icon"
+          @click="handlerClick(i, account_txa)" />
 
-    <div class="row">
-      <div class="col">
-
-        <q-item>
-          <q-item-section>
-            <div class="text-grey-8 q-gutter-xs">
-              <q-btn class="gt-xs" flat dense round v-for="(item, i) in botons" :key="i" :icon="item.icon"
-                @click="handlerClick(i, account_txa)" />
-
-              <q-btn-dropdown split push glossy no-caps icon="send" @click="onMainClick">
-                <q-list>
-                  <q-item clickable v-close-popup v-for="(item, i) in opcEmail" :key="i" @click="onItemClick">
-                    <q-item-section avatar>
-                      <q-icon :name="item.icon" />
-                    </q-item-section>
-                    <q-item-section>{{ item.label }}</q-item-section>
-                  </q-item>
-                </q-list>
-              </q-btn-dropdown>
-            </div>
-          </q-item-section>
-        </q-item>
-
-
+        <q-btn-dropdown split no-caps icon="send">
+          <q-list>
+            <q-item clickable v-close-popup v-for="(item, i) in opcEmail" :key="i"
+              @click="handlerClickMail(i, account_txa)">
+              <q-item-section avatar>
+                <q-icon :name="item.icon" />
+              </q-item-section>
+              <q-item-section>{{ item.label }}</q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </div>
-      <div class="col">
-        ....
-      </div>
-    </div>
-  </div>
+    </q-item-section>
+  </q-item>
+
+
+
 
   <div v-for="(item, i) in botons" :key="i">
 
@@ -74,17 +65,19 @@ export default {
         label: 'Ficha',
         icon: 'visibility',
         click: () => {
-          // integracionesStore.dialogoAvance = true
+          //integracionesStore.dialogoFichaCliente = true
+
         }
       }
     ]
 
     const opcEmail = [
       {
-        label: 'Ficha Avance',
+        label: 'Ficha cliente',
         icon: 'add_circle_outline',
         click: () => {
-          integracionesStore.dialogoAvance = true
+          console.log('click')
+          integracionesStore.dialogoFichaCliente = true
         }
       },
       {
@@ -100,6 +93,7 @@ export default {
       botons,
       opcEmail,
       integracionesStore,
+
       /* onItemClick,
       onMainClick */
     }
@@ -110,6 +104,13 @@ export default {
       // send variables to dialog
       this.integracionesStore.account_txa = account_txa
       this.botons[index].click()
+
+    },
+
+    handlerClickMail(index, account_txa) {
+      // send variables to dialog
+      this.integracionesStore.account_txa = account_txa
+      this.opcEmail[index].click()
 
     }
   },
